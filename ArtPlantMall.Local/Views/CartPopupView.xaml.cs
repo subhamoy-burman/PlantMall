ab9d70f71a116ed3237cabd42a6bb7ebbc2eaddf
Subhamoy Burman <subhamoy.burman@molgroup.com>
2019-12-21 14:18:20 +0530

    solution

---
+++ ArtPlantMall.Local/Views/CartPopupView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ArtPlantMall.Local.Views
+{
+    public partial class CartPopupView : ContentView
+    {
+        public CartPopupView()
+        {
+            InitializeComponent();
+        }
+
+        public double PageHeader { get; set; } = 70;
+
+        private void OnExpandTapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
